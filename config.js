5ad894cf3c35ff587cedff9191987cfc2ed40322
Matthew-Kotov <matveykotov2003@yandex.ru>
2025-12-03 13:04:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,21 @@ const CONFIG = {
     // Настройки NextGIS Web
     NGW_URL: 'https://apartmentfilter.nextgis.com',
     LAYER_IDS: {
-        SALE: 5,           // modified_data (продажа)
-        RENT: 7,           // modified_data_rent (аренда)
-        SCHOOLS: 3,        // Школы csv
-        KINDERGARTENS: 9,  // Детские сады csv
+        SALE: 58,           // modified_data (продажа)
+        RENT: 54,           // modified_data_rent (аренда)
+        SCHOOLS: 56,        // Школы csv
+        KINDERGARTENS: 60,  // Детские сады csv
         BASEMAP: 19        // OpenStreetMap
     },
     
     // Настройки карты
     MAP_CENTER: [47.2313, 39.7233], // Центр Ростова-на-Дону [lat, lng]
     MAP_ZOOM: 12,
+
+    CITY_BOUNDS: {
+        northEast: [47.35, 39.85],  // Северо-восточный угол
+        southWest: [47.15, 39.55]   // Юго-западный угол
+    },
     
     // Стили слоев
     STYLES: {
@@ -22,4 +27,5 @@ const CONFIG = {
         KINDERGARTENS: { color: 'orange', radius: 8 },
         BUFFER: { color: 'rgba(0, 0, 255, 0.2)', weight: 2 }
     }
+
 };
