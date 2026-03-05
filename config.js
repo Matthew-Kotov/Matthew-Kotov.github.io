a2065cbd937e23e2978f27b563ec7e05cda9ff82
Matthew-Kotov <matveykotov2003@yandex.ru>
2026-03-06 01:16:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-// Настройки NextGIS Web
+// config.js - настройки приложения
+const CONFIG = {
+    // Настройки NextGIS Web
     NGW_URL: 'https://apartmentfilter.nextgis.com',
     LAYER_IDS: {
         SALE: 5,
@@ -9,7 +11,7 @@
         STOPS: 11,             // добавьте правильный ID
         BASEMAP: 19
     },
-
+    
     // Настройки карты
     MAP_CENTER: [47.2313, 39.7233],
     MAP_ZOOM: 12,
@@ -18,9 +20,10 @@
         northEast: [47.35, 39.85],
         southWest: [47.15, 39.55]
     },
-
+    
     // Стили слоев
-@@ -25,6 +27,8 @@ const CONFIG = {
+    STYLES: {
+        SALE: { color: 'blue', radius: 6 },
         RENT: { color: 'green', radius: 6 },
         SCHOOLS: { color: 'red', radius: 8 },
         KINDERGARTENS: { color: 'orange', radius: 8 },
@@ -29,16 +32,12 @@
         BUFFER: { color: 'rgba(0, 0, 255, 0.2)', weight: 2 }
     },
 
-@@ -34,19 +38,10 @@ const CONFIG = {
+    // Настройки списка
+    LIST_SETTINGS: {
+        ITEMS_PER_PAGE: 50,
         SORT_OPTIONS: ['price_asc', 'price_desc', 'area_asc', 'area_desc']
     },
-
-
-
-
-
-
-
+    
     // Настройки экспорта
     EXPORT_SETTINGS: {
         FILENAME_PREFIX: 'Квартиры_',
