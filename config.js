319097b3bf041a85c971fc32cd1e4b19e3c55f3c
Matthew-Kotov <matveykotov2003@yandex.ru>
2026-03-05 23:11:43 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,20 +3,22 @@ const CONFIG = {
     // Настройки NextGIS Web
     NGW_URL: 'https://apartmentfilter.nextgis.com',
     LAYER_IDS: {
-        SALE: 5,           // modified_data (продажа) 58
-        RENT: 7,           // modified_data_rent (аренда) 54
-        SCHOOLS: 3,        // Школы csv 56
-        KINDERGARTENS: 9,  // Детские сады csv 60
-        BASEMAP: 19        // OpenStreetMap
+        SALE: 5,
+        RENT: 7,
+        SCHOOLS: 3,
+        KINDERGARTENS: 9,
+        HOSPITALS: 10,        // добавьте правильный ID
+        STOPS: 11,             // добавьте правильный ID
+        BASEMAP: 19
     },
     
     // Настройки карты
-    MAP_CENTER: [47.2313, 39.7233], // Центр Ростова-на-Дону [lat, lng]
+    MAP_CENTER: [47.2313, 39.7233],
     MAP_ZOOM: 12,
 
     CITY_BOUNDS: {
-        northEast: [47.35, 39.85],  // Северо-восточный угол
-        southWest: [47.15, 39.55]   // Юго-западный угол
+        northEast: [47.35, 39.85],
+        southWest: [47.15, 39.55]
     },
     
     // Стили слоев
@@ -25,15 +27,11 @@ const CONFIG = {
         RENT: { color: 'green', radius: 6 },
         SCHOOLS: { color: 'red', radius: 8 },
         KINDERGARTENS: { color: 'orange', radius: 8 },
+        HOSPITALS: { color: '#dc3545', radius: 8 },    // красный
+        STOPS: { color: '#6f42c1', radius: 6 },        // фиолетовый
         BUFFER: { color: 'rgba(0, 0, 255, 0.2)', weight: 2 }
     },
 
-    // Настройки списка
-    LIST_SETTINGS: {
-        ITEMS_PER_PAGE: 50,
-        SORT_OPTIONS: ['price_asc', 'price_desc', 'area_asc', 'area_desc']
-    },
-    
     // Настройки списка
     LIST_SETTINGS: {
         ITEMS_PER_PAGE: 50,
@@ -47,6 +45,3 @@ const CONFIG = {
         SHEET_NAME: 'Квартиры'
     }
 };
-
-
-
